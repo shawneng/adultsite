014381784988e8c5e04656a733dbbb74475916fb
anton <anton.romanyuk.2011@mail.ru>
2017-07-30 20:54:37 +0300

    Predator304

--- admin.php
+++ admin.php
@@ -5,7 +5,6 @@
  * Date: 30.07.2017
  * Time: 19:10
  */
-
 ?>
 <!doctype html>
 <html lang="en">
@@ -21,7 +20,24 @@
 
 <div class="head">
     <h1>Admin Panel</h1>
+    <div class="viewSite"><a href="/">Просмотыр сайта</a></div>
+
 </div>
 
+<div class="fastBlock">
+    <div class="viewUsers">
+        <span>Пользователей: </span>
+        <?php
+        //Змінна користувачів
+        ?>
+    </div>
+    <div class="cache">
+        <span>Кэш: </span>
+        <?php
+        //Змінна Кеша
+        ?>
+    </div>
+    <div class="addNews">Add News</div>
+</div>
 </body>
 </html>
