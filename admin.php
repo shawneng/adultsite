bf0f0a2a6b6db82e1425740f6b58eedb7c38d0be
anton <anton.romanyuk.2011@mail.ru>
2017-07-30 21:17:47 +0300

    Predator304

--- admin.php
+++ admin.php
@@ -5,6 +5,13 @@
  * Date: 30.07.2017
  * Time: 19:10
  */
+
+require_once 'engine/db_config.php';
+$connect_DB = mysqli_connect($hostDB, $userDB, $passwordDB, $nameDB) or die("Ошибка" . mysqli_error($connect_DB));
+
+$sql = "SELECT `id` FROM `users`";
+$query = mysqli_query($connect_DB, $sql);
+$row_num = mysqli_num_rows($query);
 ?>
 <!doctype html>
 <html lang="en">
@@ -28,7 +35,7 @@
     <div class="viewUsers inline-block">
         <span>Пользователей: </span>
         <?php
-        //Змінна користувачів
+        echo $row_num;
         ?>
     </div>
     <div class="viewCache inline-block">
