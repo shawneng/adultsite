8361d71ae8a930d4464f0ccbaf0ed39b7149031f
anton <anton.romanyuk.2011@mail.ru>
2017-07-30 20:19:28 +0300

    Predator304 Create admin.php

--- admin.php
+++ admin.php
@@ -1,12 +1,23 @@
+<?php
+/**
+ * Created by Predator304.
+ * User: anton
+ * Date: 30.07.2017
+ * Time: 19:10
+ */
+echo '<h1>Admin Panel</h1>';
+
+?>
 <!doctype html>
-<html lang="ru">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php //$title ?></title>
-    <?php require_once "style.php" ?>
+    <title>Document</title>
 </head>
 <body>
 
+</body>
+</html>
--- robutb/checkFile.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: anton
- * Date: 30.07.2017
- * Time: 16:30
- */
