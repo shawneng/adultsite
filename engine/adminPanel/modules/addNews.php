f7cfd127fd797191ea1a5c78630a79a8d55732cf
anton <anton.romanyuk.2011@mail.ru>
2017-08-01 01:49:20 +0300

    Edit Admin panel

--- engine/adminPanel/modules/addNews.php
+++ engine/adminPanel/modules/addNews.php
@@ -29,10 +29,56 @@ if (($_COOKIE['logining'] != 2) || ($_COOKIE['userStatus'] != 2)) {
 <div class="addPanel">
     <div class="editTitle">
         <span>Title: </span>
-        <label>
-            <input name="title" class="inputTitle">
-        </label>
-        <span>Max: 160</span>
+        <div>
+            <label>
+                <input name="title" class="inputTitle">
+            </label>
+        </div>
+    </div>
+
+    <div class="editLink">
+        <span>Link video: </span>
+        <div>
+            <label>
+                <input name="videoLink" class="inputVideoLink">
+            </label>
+        </div>
+    </div>
+
+    <div class="editDescription">
+        <span>Description: </span>
+        <div>
+            <label>
+                <input name="description" class="inputDescription">
+            </label>
+        </div>
+    </div>
+
+    <div class="editKeywords">
+        <span>Keywords: </span>
+        <div>
+            <label>
+                <input name="keywords" class="inputKeywords">
+            </label>
+        </div>
+    </div>
+
+    <div class="editTime">
+        <span>Time: </span>
+        <div>
+            <label>
+                <input name="time" class="inputTime">
+            </label>
+        </div>
+    </div>
+
+    <div class="editScreenLink">
+        <span>Screen link</span>
+        <div>
+            <label>
+                <input name="screenLink" class="inputScreenLink">
+            </label>
+        </div>
     </div>
 
 </div>
