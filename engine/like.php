a47e7aeed353de47309b2d4f13f853c8612ca7ad
shawneng <markpetrov1991@gmail.com>
2017-08-01 15:35:54 +0300

    update like

--- engine/like.php
+++ engine/like.php
@@ -5,10 +5,33 @@ require_once 'db_config.php';
 // Connectin to DB and check
 $connect_DB = mysqli_connect($hostDB, $userDB, $passwordDB, $nameDB) or die("Ошибка" . mysqli_error($connect_DB));
 $id_post = $_GET['id'];
+$id_user = $_GET['iduser'];
+$id_post_k = $id_post . ",";
 $sql = "SELECT likes FROM posts WHERE id = '$id_post'";
 $query = mysqli_query($connect_DB, $sql);
 $row = mysqli_fetch_array($query);
-$likes = $row[0] + 1;
+$sql = "SELECT likes_video FROM users WHERE id = '$id_user'";
+$query = mysqli_query($connect_DB, $sql);
+$likes_array = mysqli_fetch_array($query);
+$likes = $likes_array[0];
+$new_like = $likes . $id_post . ', ';
+if($_GET['like'] == 1) {
+    $likes = $row[0] - 1;
+    $new_like_array = explode(" ", $new_like);
+    $key = array_search($id_post_k , $new_like_array);
+    unset($new_like_array[$key]);
+    $key = array_search($id_post_k , $new_like_array);
+    unset($new_like_array[$key]);
+    $new_like_array = array_values($new_like_array);
+    $new_like_s = implode(" ", $new_like_array);
+    $sql = "UPDATE `users` SET `likes_video` = '$new_like_s' WHERE `users`.`id` = '$id_user'";
+    $query = mysqli_query($connect_DB, $sql);
+}
+if ($_GET['like'] == 2) {
+    $likes = $row[0] + 1;
+    $sql = "UPDATE `users` SET `likes_video` = '$new_like' WHERE `users`.`id` = '$id_user'";
+    $query = mysqli_query($connect_DB, $sql);
+}
 $sql = "UPDATE `posts` SET `likes` = '$likes' WHERE `posts`.`id` = '$id_post'";
 $query = mysqli_query($connect_DB, $sql);
 header("Location: ../");
--- templete/short-story.php
+++ templete/short-story.php
@@ -1,4 +1,9 @@
 <?php
+$sql = "SELECT likes_video FROM users WHERE id = '$id'";
+$query = mysqli_query($connect_DB, $sql);
+$likes_array = mysqli_fetch_array($query);
+$likes = $likes_array[0];
+$like_array = explode(' ', $likes);
 $sql = "SELECT `id` FROM `posts`";
 $query = mysqli_query($connect_DB, $sql);
 $row_num = mysqli_num_rows($query);
@@ -9,6 +14,7 @@ $maxId = $maxId_array[0];
 echo '<div class="content">';
 for ($i = 0; $i < $row_num; $i++) {
     $id_post = $maxId - $i;
+    $id_post_k = $id_post . ",";
     $sql = "SELECT * FROM `posts` WHERE `id` = '$id_post'";
     $query = mysqli_query($connect_DB, $sql);
     $info_array = mysqli_fetch_assoc($query);
@@ -27,10 +33,18 @@ for ($i = 0; $i < $row_num; $i++) {
                 <div class="sinfo">
                     <div class="sviews inline-block"><img src="../templete/images/views.png" alt="" class="infopng"><div class="itext inline-block">'.$info_array['views'].'</div></div>
                     <div class="stime inline-block"><img src="../templete/images/time.png" alt="" class="infopng"><div class="itext inline-block">'.$info_array['time'].'</div></div>
-                    <div class="slikes inline-block right"><a href="/engine/like.php?id='. $id_post  .'&iduser='. $id .'"><img src="../templete/images/un_like.png" alt="" class="infopngr"></a>'.$info_array['likes'].'</div>
+                    <div class="slikes inline-block right">';
+    if (in_array($id_post_k, $like_array)){
+        echo '<a href="/engine/like.php?id='. $id_post  .'&iduser='. $id .'&like=1"><img src="../templete/images/like.png" alt="" class="infopngr"></a>';
+    }
+    else{
+        echo '<a href="/engine/like.php?id='. $id_post  .'&iduser='. $id .'&like=2"><img src="../templete/images/un_like.png" alt="" class="infopngr"></a>';
+    }
+
+    echo $info_array['likes'].'</div>
                 </div>
             </div>
-';
+    ';
 }
 echo '</div>';
 ?>
