b689dd6ff911e13522ef1c1a537c7d3711b06c42
shawneng <markpetrov1991@gmail.com>
2017-08-11 20:14:24 +0300

    update db

--- dbsite.sql
+++ dbsite.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Авг 09 2017 г., 20:12
+-- Время создания: Авг 11 2017 г., 20:13
 -- Версия сервера: 5.5.53
 -- Версия PHP: 7.1.0
 
@@ -34,7 +34,7 @@ CREATE TABLE `categories` (
   `videos` int(11) NOT NULL,
   `keywords` text NOT NULL,
   `avatar` varchar(255) NOT NULL,
-  `posts` text NOT NULL
+  `posts` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -42,8 +42,9 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `description`, `title`, `name`, `videos`, `keywords`, `avatar`, `posts`) VALUES
-(2, 'вапва', 'ывавап', 'Русское порно', 0, 'вапва', 'http://i1.perdos.me/files/video/2016/12/P7039/P7039_perdos.ru_19.jpg', '1, 3, '),
-(6, 'dfgsdgs', 'dfgdfg', 'Анал', 0, 'dfgsdg', 'http://i1.perdos.me/files/photo/2015/12/P4760/P4760_perdos.ru_14493187770.jpg', '2, ');
+(1, '', '', 'Домашнее порно', 0, '', 'http://jizzcandancex.com/uploads/posts/2013-12/1387457349_ieqvwqenndzgqx06s8d.jpg', ''),
+(2, '', '', 'Анал', 0, '', 'http://i1.perdos.me/files/photo/2015/12/P4760/P4760_perdos.ru_14493187770.jpg', ''),
+(3, '', '', 'Русское порно', 0, '', 'http://i1.perdos.me/files/video/2016/12/P7039/P7039_perdos.ru_19.jpg', '');
 
 -- --------------------------------------------------------
 
@@ -60,17 +61,49 @@ CREATE TABLE `posts` (
   `views` int(11) NOT NULL,
   `likes` int(11) NOT NULL,
   `video` varchar(50) NOT NULL,
-  `keywords` text NOT NULL
+  `keywords` text NOT NULL,
+  `categories` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `posts`
 --
 
-INSERT INTO `posts` (`id`, `title`, `pre`, `descr`, `time`, `views`, `likes`, `video`, `keywords`) VALUES
-(1, 'Парень ебет девушку и кончает внутрь', 'http://adultsite/uploads/post_images/pre.jpg', '', '15:35', 15450, 769, 'http://cdn2.vids69.com/video/47/1/46077/46077.mp4', ''),
-(2, 'Дед выебал внучку и кончил ей на лицо', 'http://adultsite/uploads/post_images/pre2.jpg', '', '43:12', 12521, 608, 'http://cdn2.vids69.com/video/47/1/46077/46077.mp4', ''),
-(3, 'Русская девушка после секса страстно отсосал', 'http://adultsite/uploads/post_images/pre3.jpg', '', '18:01', 47, 50, 'http://cdn2.vids69.com/video/47/1/46077/46077.mp4', '');
+INSERT INTO `posts` (`id`, `title`, `pre`, `descr`, `time`, `views`, `likes`, `video`, `keywords`, `categories`) VALUES
+(1, 'Парень ебет девушку и кончает внутрь', 'http://adultsite/uploads/post_images/pre.jpg', '', '15:35', 15455, 770, 'http://cdn2.vids69.com/video/47/1/46077/46077.mp4', '', '1, 2'),
+(2, 'Дед выебал внучку и кончил ей на лицо', 'http://adultsite/uploads/post_images/pre2.jpg', '', '43:12', 12529, 609, 'http://cdn2.vids69.com/video/47/1/46077/46077.mp4', '', '1, 2, 3'),
+(3, 'Русская девушка после секса страстно отсосал', 'http://adultsite/uploads/post_images/pre3.jpg', '', '18:01', 48, 50, 'http://cdn2.vids69.com/video/47/1/46077/46077.mp4', '', '1, 3'),
+(4, 'Русская девушка после секса страстно отсосал', 'http://adultsite/uploads/post_images/pre3.jpg', '', '18:01', 48, 55, 'http://cdn2.vids69.com/video/47/1/46077/46077.mp4', '', '1, 3'),
+(5, 'Дед выебал внучку и кончил ей на лицо', 'http://adultsite/uploads/post_images/pre2.jpg', '', '43:12', 12529, 608, 'http://cdn2.vids69.com/video/47/1/46077/46077.mp4', '', '1, 2, 3'),
+(6, 'Парень ебет девушку и кончает внутрь', 'http://adultsite/uploads/post_images/pre.jpg', '', '15:35', 15455, 770, 'http://cdn2.vids69.com/video/47/1/46077/46077.mp4', '', '1, 2'),
+(7, 'Русская девушка после секса страстно отсосал', 'http://adultsite/uploads/post_images/pre3.jpg', '', '18:01', 48, 50, 'http://cdn2.vids69.com/video/47/1/46077/46077.mp4', '', '1, 3'),
+(8, 'Парень ебет девушку и кончает внутрь', 'http://adultsite/uploads/post_images/pre.jpg', '', '15:35', 15455, 771, 'http://cdn2.vids69.com/video/47/1/46077/46077.mp4', '', '1, 2'),
+(9, 'Дед выебал внучку и кончил ей на лицо', 'http://adultsite/uploads/post_images/pre2.jpg', '', '43:12', 12529, 608, 'http://cdn2.vids69.com/video/47/1/46077/46077.mp4', '', '1, 2, 3'),
+(10, 'Парень ебет девушку и кончает внутрь', 'http://adultsite/uploads/post_images/pre.jpg', '', '15:35', 15455, 770, 'http://cdn2.vids69.com/video/47/1/46077/46077.mp4', '', '1, 2'),
+(11, 'Дед выебал внучку и кончил ей на лицо', 'http://adultsite/uploads/post_images/pre2.jpg', '', '43:12', 12529, 608, 'http://cdn2.vids69.com/video/47/1/46077/46077.mp4', '', '1, 2, 3'),
+(12, 'Дед выебал внучку и кончил ей на лицо', 'http://adultsite/uploads/post_images/pre2.jpg', '', '43:12', 12529, 608, 'http://cdn2.vids69.com/video/47/1/46077/46077.mp4', '', '1, 2, 3'),
+(13, 'Парень ебет девушку и кончает внутрь', 'http://adultsite/uploads/post_images/pre.jpg', '', '15:35', 15456, 770, 'http://cdn2.vids69.com/video/47/1/46077/46077.mp4', '', '1, 2'),
+(14, 'Русская девушка после секса страстно отсосал', 'http://adultsite/uploads/post_images/pre3.jpg', '', '18:01', 49, 51, 'http://cdn2.vids69.com/video/47/1/46077/46077.mp4', '', '1, 3');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `settings`
+--
+
+CREATE TABLE `settings` (
+  `id` int(11) NOT NULL,
+  `name_site` text NOT NULL,
+  `descr_site` text NOT NULL,
+  `keywords` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `settings`
+--
+
+INSERT INTO `settings` (`id`, `name_site`, `descr_site`, `keywords`) VALUES
+(1, 'adult2', 'sdfsdf', 'sdfsdf');
 
 -- --------------------------------------------------------
 
@@ -94,7 +127,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `login`, `password`, `email`, `status_user`, `avatar`, `likes_video`, `history`) VALUES
-(1, 'shawn', 'Shawn1Shawn', 'markpetrov1991@gmail.com', 2, 'http://adultsite/uploads/avatars/1.jpg', '3, ', '3,  1, '),
+(1, 'shawn', 'Shawn1Shawn', 'markpetrov1991@gmail.com', 2, 'http://adultsite/uploads/avatars/1.jpg', '3, 1, 8, 2, 4, 4, 4, 4, 4, 14, ', '3,  1,  2,  13,  14, '),
 (2, 'predator', '123', '123@gmai.com', 2, '', '', ''),
 (3, 'bob', 'qwe', 'qwe@gmail.com', 1, '', '', ''),
 (4, 'bobo', 'qwerty', 'markshysholik@gmail.com', 1, '', '3, ', ''),
@@ -117,6 +150,12 @@ ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Индексы таблицы `settings`
+--
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Индексы таблицы `users`
 --
 ALTER TABLE `users`
@@ -135,7 +174,12 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT для таблицы `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+--
+-- AUTO_INCREMENT для таблицы `settings`
+--
+ALTER TABLE `settings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
