8b998f7ba4f68177c8125b49a8de4a7665cde07e
anton <anton.romanyuk.2011@mail.ru>
2017-08-13 20:56:31 +0300

    main

--- templete/main.php
+++ templete/main.php
@@ -13,9 +13,11 @@ $sql = "SELECT `history` FROM `users` WHERE id = '$id'";
 $query = mysqli_query($connect_DB, $sql);
 $history_video_a = mysqli_fetch_array($query);
 $history_video_id = explode(', ', $history_video_a[0]);
+
 $num_videos = count($history_video_id);
 $num_videos = $num_videos-1;
 $numh = $num_videos;
+
 $sql = "SELECT max(id) as id FROM `categories`";
 $query = mysqli_query($connect_DB, $sql);
 $num_cat = mysqli_fetch_array($query);
