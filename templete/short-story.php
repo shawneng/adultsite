ff6cf10590c5ac73b40ecaaeeec181c8e5973276
anton <anton.romanyuk.2011@mail.ru>
2017-08-13 19:48:38 +0300

    news

--- templete/short-story.php
+++ templete/short-story.php
@@ -6,63 +6,65 @@ $likes_array = mysqli_fetch_array($query);
 $likes = $likes_array[0];
 $like_array = explode(' ', $likes);
 // Вывод видео
-$sql = "SELECT `id` FROM `posts`";
+
+echo '<div class="content">';
+
+$sql = "SELECT * FROM `posts` ORDER BY `posts`.`id` DESC";
 $query = mysqli_query($connect_DB, $sql);
+$info_array = mysqli_fetch_assoc($query);
+
 $row_num = mysqli_num_rows($query);
-$sql = "SELECT max(`id`) as `id` FROM `posts`";
-$query = mysqli_query($connect_DB, $sql);
-$maxId_array = mysqli_fetch_array($query);
-$maxId = $maxId_array[0];
+
+$maxId = $info_array['id'];
+
 $pages = $row_num / 10;
 $pages = (int)$pages;
 $pages++;
-echo '<div class="content">';
-for ($i = 0; $i < 18 ; $i++) {
+
+for ($i = 0; $i < 18; $i++) {
     $id_post = $maxId - $i;
     $id_post_k = $id_post . ",";
-    $sql = "SELECT * FROM `posts` WHERE `id` = '$id_post'";
-    $query = mysqli_query($connect_DB, $sql);
     $info_array = mysqli_fetch_assoc($query);
+
     echo '
     <div class="row1 z-depth-3">
-        <a href="/?id='. $id_post .'">
+        <a href="/?id=' . $id_post . '">
             <div class="card">
                 <div class="card-image waves-effect waves-block waves-light">
-                    <img src="'.$info_array['pre'].'" alt="">
+                    <img src="' . $info_array['pre'] . '" alt="">
                 </div>
             </div>
         </a>
         <div class="card-content center">
-        <span class="card-title">'.$info_array['title'].'</span>
+        <span class="card-title">' . $info_array['title'] . '</span>
     </div>
                 <div class="sinfo">
-                    <div class="sviews inline-block"><i class="tiny material-icons">visibility</i><div class="itext inline-block">'.$info_array['views'].'</div></div>
-                    <div class="stime inline-block"><i class="tiny material-icons">access_time</i><div class="itext inline-block">'.$info_array['time'].'</div></div>
+                    <div class="sviews inline-block"><i class="tiny material-icons">visibility</i><div class="itext inline-block">' . $info_array['views'] . '</div></div>
+                    <div class="stime inline-block"><i class="tiny material-icons">access_time</i><div class="itext inline-block">' . $info_array['time'] . '</div></div>
                     <div class="inline-block right">';
-    if ($_COOKIE['logining'] == 2) {
-        if (in_array($id_post_k, $like_array)){
-            echo '<a href="/engine/like.php?id='. $id_post  .'&iduser='. $id .'&like=1"><i class="tiny material-icons">favorite</i></a>';
-        }
-        else{
-            echo '<a href="/engine/like.php?id='. $id_post  .'&iduser='. $id .'&like=2"><i class="tiny material-icons">favorite_border</i></a>';
-        }
-    }
-    elseif ($_COOKIE['logining'] !== 2) {
-        echo '<a href="/"><img src="../templete/images/un_like.png" alt="" class="infopngr"></a>';
-    }
 
-    echo '<div class="itext inline-block">'.$info_array['likes'].'</div></div>
+        if ($_COOKIE['logining'] == 2) {
+            if (in_array($id_post_k, $like_array)) {
+                echo '<a href="/engine/like.php?id=' . $id_post . '&iduser=' . $id . '&like=1"><i class="tiny material-icons">favorite</i></a>';
+            } else {
+                echo '<a href="/engine/like.php?id=' . $id_post . '&iduser=' . $id . '&like=2"><i class="tiny material-icons">favorite_border</i></a>';
+            }
+        } elseif ($_COOKIE['logining'] !== 2) {
+            echo '<a href="/"><img src="../templete/images/un_like.png" alt="" class="infopngr"></a>';
+        }
+
+        echo '<div class="itext inline-block">' . $info_array['likes'] . '</div></div>
                 </div>
             </div>
     ';
+
 }
 echo '<ul class="pagination">';
 $active_page = (int)$_GET['page'];
-for ($i = 1; $i <= $pages; $i++){
-    if ( $i == 1 ) {
-        echo '<li class="active"><a href="?page='.$i.'">'.$i.'</a></li>';
-    }
-    else {
+for ($i = 1; $i <= $pages; $i++) {
+    if ($i == 1) {
+        echo '<li class="active"><a href="?page=' . $i . '">' . $i . '</a></li>';
+    } else {
         echo '
         <li class="waves-effect"><a href="?page=' . $i . '">' . $i . '</a></li>
         ';
